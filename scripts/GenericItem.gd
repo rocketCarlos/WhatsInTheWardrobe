44948e9b259f69a9b27c9a0b616c22cfb3730a3b
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2024-11-20 18:05:23 +0100

    Completed experimental GenericItem

--- scripts/GenericItem.gd
+++ scripts/GenericItem.gd
@@ -1,24 +1,32 @@
 extends Sprite2D
 
-var main_sprite = load("res://icon.svg")
-var second_sprite = load("res://icon.svg")
+var main_sprite = load("res://assets/interactive objects/drawer_closed.png")
+var second_sprite = load("res://assets/interactive objects/drawer_opened.png")
+var current_sprite
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	current_sprite = 0
+	texture = main_sprite
+	$Bright.texture = main_sprite
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Interact"):
+		if current_sprite == 0:
+			texture = second_sprite
+			$Bright.texture = second_sprite 
+			current_sprite = 1
+		elif current_sprite == 1:
+			texture = main_sprite
+			$Bright.texture = main_sprite
+			current_sprite = 0
 
 
 func _on_area_2d_mouse_entered() -> void:
-	texture = second_sprite
-	$Bright.texture = second_sprite
 	$Bright.show()
 
 
 func _on_area_2d_mouse_exited() -> void:
-	texture = main_sprite
-	$Bright.texture = main_sprite
 	$Bright.hide()
