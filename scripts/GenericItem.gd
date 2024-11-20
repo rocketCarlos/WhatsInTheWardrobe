cc1a3fa607a399fe8d08ec9edfc1b88c27b7b171
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2024-11-21 00:10:17 +0100

    Refactored GenericItem and created mockScene

--- scripts/GenericItem.gd
+++ scripts/GenericItem.gd
@@ -1,51 +1,67 @@
 extends Sprite2D
 
-var main_collider
-var main_sprite:
-	set(path):
-		main_sprite = load(path)
-		var collider = RectangleShape2D.new()
-		collider.size = Vector2(main_sprite.get_width(), main_sprite.get_height())
-		main_collider = collider
-		
-var second_collider
-var second_sprite:
-	set(path):
-		second_sprite = load(path)
-		var collider = RectangleShape2D.new()
-		collider.size = Vector2(second_sprite.get_width(), second_sprite.get_height())
-		second_collider = collider
-		
-var current_sprite
+#region attributes
+# Indicates the current sprite displayed. 0 == closed, 1 == open. Setting current_sprite to 0 or 1 
+# automatically makes the object show the corresponding sprite
+var current_sprite:
+	set(which):
+		current_sprite = which
+		if current_sprite == 0:
+			texture = sprite_closed
+			$Bright.texture = sprite_closed
+			$Area2D/CollisionShape2D.shape = collider_closed
+		elif current_sprite == 1:
+			texture = sprite_open
+			$Bright.texture = sprite_open
+			$Area2D/CollisionShape2D.shape = collider_open
+		else:
+			push_error("Invalid sprite selected in node ", name)
 
+# Represents the sprite displayed when the item is closed and generates its collision shape
+var collider_closed
+var sprite_closed:
+	set(path):
+		sprite_closed = load(path)
+		var collider = RectangleShape2D.new()
+		collider.size = Vector2(sprite_closed.get_width(), sprite_closed.get_height())
+		collider_closed = collider
+		
+		current_sprite = 0
 
+# Represents the sprite displayed when the item is open and generates its collision shape
+var collider_open
+var sprite_open:
+	set(path):
+		sprite_open = load(path)
+		var collider = RectangleShape2D.new()
+		collider.size = Vector2(sprite_open.get_width(), sprite_open.get_height())
+		collider_open = collider
+		
+		current_sprite = 1
+		
+
+var mouse_in_collider
+#endregion
+
+#region ready and process
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	main_sprite = "res://assets/interactive objects/mom_wardrobe.png"
-	second_sprite = "res://assets/interactive objects/mom_wardrobe_opened.png"
-	current_sprite = 0
-	texture = main_sprite
-	$Bright.texture = main_sprite
-	$Area2D/CollisionShape2D.shape = main_collider
+	mouse_in_collider = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("Interact"):
-		if current_sprite == 0:
-			texture = second_sprite
-			$Bright.texture = second_sprite 
-			$Area2D/CollisionShape2D.shape = second_collider
-			current_sprite = 1
-		elif current_sprite == 1:
-			texture = main_sprite
-			$Bright.texture = main_sprite
-			$Area2D/CollisionShape2D.shape = main_collider
-			current_sprite = 0
+	# toggles between open and closed sprites 
+	if Input.is_action_just_pressed("Interact") and mouse_in_collider:
+		current_sprite = (current_sprite + 1) % 2
+#endregion
 
-
+#region signal functions
 func _on_area_2d_mouse_entered() -> void:
 	$Bright.show()
+	mouse_in_collider = true
 
 
 func _on_area_2d_mouse_exited() -> void:
 	$Bright.hide()
+	mouse_in_collider = false
+#endregion
