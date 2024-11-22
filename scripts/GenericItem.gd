74fb1caeefb488acdafee92109f104c272d3bd69
Carlos Muñoz Sánchez <mscarlos.contact@gmail.com>
2024-11-22 01:17:07 +0100

    Added hierarchy system for generic item

--- scripts/GenericItem.gd
+++ scripts/GenericItem.gd
@@ -1,52 +1,100 @@
 extends Sprite2D
 
+#region scene nodes
+@onready var highlight = $Highlight
+@onready var hitbox = $Hitbox
+@onready var hitbox_shape_closed = $Hitbox/ShapeClosed
+@onready var hitbox_shape_open = $Hitbox/ShapeOpen
+
+#endregion
+
 #region attributes
 # Indicates the current sprite displayed. 0 == closed, 1 == open. Setting current_sprite to 0 or 1 
-# automatically makes the object show the corresponding sprite
+# automatically makes the object show the corresponding sprite and collision shape
 var current_sprite:
 	set(which):
 		current_sprite = which
+		
 		if current_sprite == 0:
 			texture = sprite_closed
-			$Bright.texture = sprite_closed
-			$Area2D/CollisionShape2D.shape = collider_closed
+			highlight.texture = sprite_closed
+			
+			hitbox_shape_closed.set_deferred(&"disabled", false)
+			hitbox_shape_open.set_deferred(&"disabled", true)
+			
+			# tell children to be invisible
+			for child in children:
+				child.invisible = true
+			
 		elif current_sprite == 1:
 			texture = sprite_open
-			$Bright.texture = sprite_open
-			$Area2D/CollisionShape2D.shape = collider_open
+			highlight.texture = sprite_open
+			
+			hitbox_shape_closed.set_deferred(&"disabled", true)
+			hitbox_shape_open.set_deferred(&"disabled", false)
+			
+			# tell children to be visible again
+			for child in children:
+				child.invisible = false
+			
 		else:
 			push_error("Invalid sprite selected in node ", name)
 
 # Represents the sprite displayed when the item is closed and generates its collision shape
-var collider_closed
-var sprite_closed:
-	set(path):
-		sprite_closed = load(path)
+@export var sprite_closed: Texture2D:
+	set(asset):
+		sprite_closed = asset
 		var collider = RectangleShape2D.new()
 		collider.size = Vector2(sprite_closed.get_width(), sprite_closed.get_height())
-		collider_closed = collider
 		
-		current_sprite = 0
-
+		await ready
+		hitbox_shape_closed.shape = collider
+		
 # Represents the sprite displayed when the item is open and generates its collision shape
-var collider_open
-var sprite_open:
-	set(path):
-		sprite_open = load(path)
+@export var sprite_open: Texture2D:
+	set(asset):
+		sprite_open = asset
 		var collider = RectangleShape2D.new()
 		collider.size = Vector2(sprite_open.get_width(), sprite_open.get_height())
-		collider_open = collider
 		
-		current_sprite = 1
-		
-
+		await ready
+		hitbox_shape_open.shape = collider
+				
 var mouse_in_collider
+
+@export var parent: Node
+@export var children: Array[Node]
+
+# if disabled, the item cannot be selected or interacted with
+# used when the mouse is already selecting an son of this object
+var disabled:
+	set(value):
+		if current_sprite == 0:
+			hitbox_shape_closed.set_deferred(&"disabled", value)
+		else:
+			hitbox_shape_open.set_deferred(&"disabled", value)
+		
+		
+# if invisible, the item cannot be selected or intereacted with and it's invisible
+# used when this object is inside another object that is closed
+var invisible:
+	set(value):
+		disabled = value
+		visible = not value
+
 #endregion
 
 #region ready and process
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	mouse_in_collider = false
+	current_sprite = 0
+	
+	disabled = false
+	if not parent == null:
+		invisible = true
+	else:
+		invisible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -57,11 +105,20 @@ func _process(delta: float) -> void:
 
 #region signal functions
 func _on_area_2d_mouse_entered() -> void:
-	$Bright.show()
+	highlight.show()
 	mouse_in_collider = true
+	
+	# tell parent to disable interactions
+	if not parent == null:
+		parent.disabled = true
 
 
 func _on_area_2d_mouse_exited() -> void:
-	$Bright.hide()
+	highlight.hide()
 	mouse_in_collider = false
+	
+	# tell parent to enable interactions
+	if not parent == null:
+		parent.disabled = false
+		
 #endregion
--- scripts/mock_scene.gd
+++ scripts/mock_scene.gd
@@ -3,11 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$MumWardrobe.sprite_closed = "res://assets/interactive objects/mom_wardrobe.png"
-	$MumWardrobe.sprite_open = "res://assets/interactive objects/mom_wardrobe_opened.png"
-	
-	$MumWardrobe/SecurityBox.sprite_closed = "res://assets/interactive objects/drawer2_closed.png"
-	$MumWardrobe/SecurityBox.sprite_open = "res://assets/interactive objects/drawer2_opened.png"
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
